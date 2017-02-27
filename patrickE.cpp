88415d5b0eeb3ac10d37bf41cfe086f4d010db58
pespirit <pespiritu@csub.edu>
2017-02-27 13:41:21 -0800

    Upgrade menu *update*

--- patrickE.cpp
+++ patrickE.cpp
@@ -14,38 +14,78 @@
 //#include "ppm.h"
 
 /*
-Ppmimage *background = NULL;
+Ppmimage *forestImage=NULL;
+GLuint forestTexture;
+int forest=1;
+forestImage	= ppm6GetImage("./images/forest.ppm");
+glGenTextures(1, &forestTexture);
 
-// convert image to ppm
-system(convert "./images/background.jpg" "./images/background.ppm");
-
-// load image files into a ppm structure
-background = ppm6GetImage("./images/background.ppm");
-
-int forest = 1;
+glBindTexture(GL_TEXTURE_2D, forestTexture);
+glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER,GL_NEAREST);
+glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER,GL_NEAREST);
+glTexImage2D(GL_TEXTURE_2D, 0,3, forestImage-> width, forestImage->height,
+	0, GL_RGB, GL_UNSIGNED_BYTE, forestImage->data);
 */
 
 void UpgradeMenu(int xres,int yres,Rect r)
 {
-    glColor3f(1,0,0);
+    glColor3f(.13,.49,.2);
     int cx = xres/2;
     int cy = yres/2;
     glBegin(GL_QUADS);
-    glVertex2i(cx-400,cy+200);
-    glVertex2i(cx+400,cy+200);
-    glVertex2i(cx+100,cy-200);
-    glVertex2i(cx-100,cy-200);
+    glVertex2i(cx-100,cy+100);
+    glVertex2i(cx+100,cy+100);
+    glVertex2i(cx+100,cy-100);
+    glVertex2i(cx-100,cy-100);
     glEnd();
     glEnable(GL_TEXTURE_2D);
-    r.bot=cy+20;
+    r.bot=cy+60;
     r.left=cx;
     r.center=1;
-    ggprint8b(&r,16,0,"Upgrades");    
+    ggprint16(&r,40,0,"Upgrades");    
+    ggprint13(&r,20,0,"Booster Uprades");    
+    ggprint13(&r,20,0,"Laser Upgrades");    
 }
 
-/*
-// background image
-glBindTexture(GL_TEXTURE_2D, background);
-glTexImage2D(GL_TEXTURE_2D,0,3, background ->width, 
-background-> height, 0, GL_RGB, GL_UNSIGNED_BYTE, background->data);
-*/
+void BoosterMenu(int xres,int yres,Rect r)
+{
+    glColor3f(.13,.49,.2);
+    int cx = xres/2;
+    int cy = yres/2;
+    glBegin(GL_QUADS);
+    glVertex2i(cx-100,cy+100);
+    glVertex2i(cx+100,cy+100);
+    glVertex2i(cx+100,cy-100);
+    glVertex2i(cx-100,cy-100);
+    glEnd();
+    glEnable(GL_TEXTURE_2D);
+    r.bot=cy+60;
+    r.left=cx;
+    r.center=1;
+    ggprint16(&r,40,0,"Booster Upgrades");    
+    ggprint13(&r,20,0,"Booster Level 1");    
+    ggprint13(&r,20,0,"Booster Level 2");    
+    ggprint13(&r,20,0,"Booster Level 3");    
+}
+
+void LaserMenu(int xres,int yres,Rect r)
+{
+    glColor3f(.13,.49,.2);
+    int cx = xres/2;
+    int cy = yres/2;
+    glBegin(GL_QUADS);
+    glVertex2i(cx-100,cy+100);
+    glVertex2i(cx+100,cy+100);
+    glVertex2i(cx+100,cy-100);
+    glVertex2i(cx-100,cy-100);
+    glEnd();
+    glEnable(GL_TEXTURE_2D);
+    r.bot=cy+60;
+    r.left=cx;
+    r.center=1;
+    ggprint16(&r,40,0,"Laser Upgrades");    
+    ggprint13(&r,20,0,"Laser Fire Speed 1");    
+    ggprint13(&r,20,0,"Laser Fire Speed 2");    
+    ggprint13(&r,20,0,"Dual Laser Fire");    
+    ggprint13(&r,20,0,"Triple Laser Fire");    
+}
