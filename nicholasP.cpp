c8a9d1e528d331436be565a89597bf5003b6e66c
npolach <npolach@csub.edu>
2017-03-23 08:13:52 -0700

    Updated some classes

--- nicholasP.cpp
+++ nicholasP.cpp
@@ -14,52 +14,13 @@
 #include "fonts.h"
 #include "nicholasP.h"
 
-//// Get this from rocketlander.cpp
-//typedef float Vec[3];
-//
-////extern struct Shape;
-//struct Shape {
-//    float width, height;
-//    float radius;
-//    Vec center;
-//};
-
-//// Objects such as: platforms
-//class Object
-//{
-////    private:
-////	Vec pos;
-////	Shape shape;
-//
-//    public:
-//	Vec pos;
-//	Shape shape;
-//
-//	Object()
-//	{
-//	    pos[0] = 0;
-//	    pos[1] = 0;
-//	    shape.width = 0.0;
-//	    shape.height = 0.0;
-//	    shape.radius = 0.0;
-//	    shape.center[0] = 0.0;
-//	    shape.center[1] = 0.0;
-//	}
-//
-//	~Object()
-//	{
-//
-//	}
-//
-//	virtual void draw() = 0;
-//};
-
 Object::Object()
 {
     pos[0] = 0;
     pos[1] = 0;
     shape.width = 0.0;
     shape.height = 0.0;
+    shape.base = 0.0;
     shape.radius = 0.0;
     shape.center[0] = 0.0;
     shape.center[1] = 0.0;
@@ -70,22 +31,7 @@ Object::~Object()
 
 }
 
-
-//// Objects such as: player, enemies, asteroids
-//class MovableObject : public Object
-//{
-//    private:
-//	Vec vel;
-//
-//    public:
-//	MovableObject()
-//	{
-//	    vel[0] = 0;
-//	    vel[1] = 0;
-//	}
-//
-//	virtual void move() = 0;
-//};
+// ---------- //
 
 MovableObject::MovableObject()
 {
@@ -93,117 +39,7 @@ MovableObject::MovableObject()
     vel[1] = 0;
 }
 
-
-//class Ship : public MovableObject {
-//
-//    private:
-//	// Choose between the 3
-//	bool haveBooster1;
-//	bool enabledBooster1;
-//	bool haveBooster2;
-//	bool enabledBooster2;
-//	bool haveBooster3;
-//	bool enabledBooster3;
-//
-//	bool haveLeftStrafe;
-//	bool haveRightStrafe;
-//	bool haveDoubleShot;
-//	bool haveTripleShot;
-//
-//    public:
-//	void move() override {
-//	    printf("The ship is moving!\n");
-//	}
-//
-//	bool collidesWith() {
-//	    printf("The ship is colliding!\n");
-//	    return true;
-//	}
-//
-//	void draw() override {
-//	    printf("Drawing the ship!\n");
-//	}
-//
-//	// Funcs to interface with Patrick's menus
-//	void buyBooster1()
-//	{
-//	    haveBooster1 = true;
-//	}	    
-//	void buyBooster2()
-//	{
-//	    haveBooster2 = true;
-//	}	    
-//	void buyBooster3()
-//	{
-//	    haveBooster3 = true;
-//	}	    
-//	void buyLeftStrafe()
-//	{
-//	    haveLeftStrafe = true;
-//	}	    
-//	void buyRightStrafe()
-//	{
-//	    haveRightStrafe = true;
-//	}	    
-//	void buyDoubleShot()
-//	{
-//	    haveDoubleShot = true;
-//	}	    
-//	void buyTripleShot()
-//	{
-//	    haveTripleShot = true;
-//	}	    
-//
-//	bool ownBooster1()
-//	{
-//	    return haveBooster1;
-//	}	    
-//	bool ownBooster2()
-//	{
-//	    return haveBooster2;
-//	}	    
-//	bool ownBooster3()
-//	{
-//	    return haveBooster3;
-//	}	    
-//	bool ownLeftStrafe()
-//	{
-//	    return haveLeftStrafe;
-//	}	    
-//	bool ownRightStrafe()
-//	{
-//	    return haveRightStrafe;
-//	}	    
-//	bool ownDoubleShot()
-//	{
-//	    return haveDoubleShot;
-//	}	    
-//	bool ownTripleShot()
-//	{
-//	    return haveTripleShot;
-//	}	    
-//
-//	void enableBooster1()
-//	{
-//	    enabledBooster2 = false;
-//	    enabledBooster3 = false;
-//	    enabledBooster1 = true;
-//	}
-//
-//	void enableBooster2()
-//	{
-//	    enabledBooster1 = false;
-//	    enabledBooster3 = false;
-//	    enabledBooster2 = true;
-//	}
-//
-//	void enableBooster3()
-//	{
-//	    enabledBooster1 = false;
-//	    enabledBooster2 = false;
-//	    enabledBooster3 = true;
-//	}
-//};
+// ---------- //
 
 Ship2::Ship2()
 {
@@ -230,6 +66,8 @@ Ship2::Ship2()
 	shape.width = collidables[0].width + collidables[1].width + collidables[2].width;
 	shape.height = collidables[0].height + collidables[3].height + collidables[4].height;
 	// Figure out how to set positions
+
+    fuel = 100;
 }
 
 void Ship2::move()
@@ -288,165 +126,93 @@ void Ship2::drawRect()
     glPopMatrix();
 }
 
+void Ship2::enableBooster1()
+{
+    enabledBooster2 = false;
+    enabledBooster3 = false;
+    enabledBooster1 = true;
+}
 
-//class Enemy : public MovableObject {
-//
-//    private:
-//	int health;
-//	//Ship * ship;
-//
-//    public:
-//	Enemy()
-//	    //Enemy(Ship * s)
-//	{
-//	    health = 100;
-//	    //ship = s;
-//	}
-//
-//	void move() override {
-//	    printf("The enemy is moving!\n");
-//	}
-//
-//	void draw() override {
-//	    printf("Drawing the enemy!\n");
-//	}
-//};
+void Ship2::enableBooster2()
+{
+    enabledBooster1 = false;
+    enabledBooster3 = false;
+    enabledBooster2 = true;
+}
 
-//Enemy::Enemy()
-//    //Enemy(Ship * s)
-//{
-//    health = 100;
-//    //ship = s;
-//}
-//
-//void Enemy::move() override {
-//    printf("The enemy is moving!\n");
-//}
-//
-//void Enemy::draw() override {
-//    printf("Drawing the enemy!\n");
-//}
+void Ship2::enableBooster3()
+{
+    enabledBooster1 = false;
+    enabledBooster2 = false;
+    enabledBooster3 = true;
+}
 
-//class Chaser : public Enemy {
-//
-//    private:
-//	Ship * ship;
-//
-//    public:
-//	Chaser(Ship * s)
-//	{
-//	    ship = s;
-//	}
-//
-//	void move() override {
-//	    printf("The chaser is moving!\n");
-//	}
-//
-//	void draw() override {
-//	    printf("Drawing the chaser!\n");
-//	}
-//};
+// ---------- //
+
+Enemy::Enemy()
+{
+    health = 100;
+}
+
+void Enemy::move() {
+    printf("The enemy is moving!\n");
+}
+
+void Enemy::draw() {
+    printf("Drawing the enemy!\n");
+}
+
+// ---------- //
 
 //Chaser::Chaser(Ship * s)
 //{
 //    ship = s;
 //}
-//
-//void Chaser::move() override {
-//    printf("The chaser is moving!\n");
-//}
-//
-//void Chaser::draw() override {
-//    printf("Drawing the chaser!\n");
-//}
 
-//class Circler : public Enemy {
-//
-//    private:
-//	Ship * ship;
-//
-//    public:
-//	Circler(Ship * s)
-//	{
-//	    ship = s;
-//	}
-//
-//	void move() override
-//	{
-//	    printf("The circler is moving!\n");
-//	}
-//
-//	void draw() override
-//	{
-//	    printf("Drawing the circler!\n");
-//	}
-//};
+void Chaser::move() {
+    printf("The chaser is moving!\n");
+}
+
+void Chaser::draw() {
+    printf("Drawing the chaser!\n");
+}
+
+// ---------- //
 
 //Circler::Circler(Ship * s)
 //{
 //    ship = s;
 //}
-//
-//void Circler::move() override
-//{
-//    printf("The circler is moving!\n");
-//}
-//
-//void Circler::draw() override
-//{
-//    printf("Drawing the circler!\n");
-//}
 
-//class Asteroid : public MovableObject {
-//
-//    public:
-//	void move() override
-//	{
-//	    printf("The asteroid is moving!\n");
-//	}
-//
-//	void draw() override
-//	{
-//	    printf("Drawing the asteroid!\n");
-//	}
-//};
+void Circler::move() 
+{
+    printf("The circler is moving!\n");
+}
 
-//void Asteroid::move() override
-//{
-//    printf("The asteroid is moving!\n");
-//}
-//
-//void Asteroid::draw() override
-//{
-//    printf("Drawing the asteroid!\n");
-//}
+void Circler::draw() 
+{
+    printf("Drawing the circler!\n");
+}
 
-//class Platform : public Object {
-//
-//    public:
-//	void draw() override
-//	{
-//	    //draw platform
-//	    glColor3ub(255,165,0);
-//	    glPushMatrix();
-//	    glTranslatef(pos[0], pos[1], 0);
-//	    glBegin(GL_QUADS);
-//
-//	    glVertex2i(-shape.width,-shape.height);
-//	    glVertex2i(-shape.width, shape.height);
-//	    glVertex2i(shape.width, shape.height);
-//	    glVertex2i(shape.width, -shape.height);
-//
-////	    glVertex2i(-shape.width,-shape.height);
-////	    glVertex2i(-shape.width, shape.height);
-////	    glVertex2i(shape.width, shape.height);
-////	    glVertex2i(shape.width, -shape.height);
-//	    glEnd();
-//	    glPopMatrix();
-//
-//	    printf("Drawing the platform!\n");
-//	}
-//};
+// ---------- //
+
+void Asteroid2::move()
+{
+    printf("The asteroid is moving!\n");
+}
+
+void Asteroid2::draw()
+{
+    printf("Drawing the asteroid!\n");
+}
+
+// ---------- //
+
+Platform::Platform()
+{
+    isGoal = false;
+    isRefuel = false;
+}
 
 void Platform::draw()
 {
@@ -472,111 +238,50 @@ void Platform::draw()
     glPopMatrix();
 }
 
-//	class Level {
-//	    private:
-//		float gravity;
-//		float time; // Use whatever type is needed
-//		int score;
-//		Ship ship;
-//		Enemy enemies[5]; // This will be changed when we add enemy types
-//		Asteroid asteroids[10]; // handle enemies like bullets in asteroid framework
-//		Platform platforms[100]; // This can be a normal array
-//
-//		// Keep track of number of enemies/asteroids (use in add/remove funcs)
-//		int enemyCount = 0;
-//		int asteroidCount = 0;
-//		//Objective objective; // Don't know how to handle this yet
-//		/*
-//		 * Other variables
-//		 * that are needed
-//		 */
-//
-//		// Functions:
-//		void loadLevel(char levelFile[]) {printf("%s\n", levelFile);} // Reads xml file and contains switch case to load objects
-//		void moveObjects() {} // Calls move() for all MovableObject (ship/enemies/asteroids)
-//		void removeEnemy(int index) {}
-//		void addEnemy(Enemy enemy) {}
-//		void addAsteroid(Asteroid ast) {}
-//		void removeAsteroid(int index) {}
-//		int calculateFinalScore() {return 0;} // ?
-//		bool objectiveComplete() {return false;} // Check if objective has been complete
-//
-//	    public:
-//		void init_level() {} // Run any initialization code
-//		void runCycle() {} // Run a single cycle of the game
-//
-//		Level(char levelFile[])
-//		{
-//		    score = 0;
-//		    loadLevel(levelFile);
-//		}
-//	};
+// ---------- //
 
-//void Level::loadLevel(char levelFile[]) {printf("%s\n", levelFile);}
-//void Level::moveObjects() {} // Calls move() for all MovableObject (ship/enemies/asteroids)
-//void Level::removeEnemy(int index) {}
-//void Level::addEnemy(Enemy enemy) {}
-//void Level::addAsteroid(Asteroid ast) {}
-//void Level::removeAsteroid(int index) {}
-//int Level::calculateFinalScore() {return 0;} // ?
-//bool Level::objectiveComplete() {return false;} // Check if objective has been complete
-//
-//void Level::init_level() {} // Run any initialization code
-//void Level::runCycle() {} // Run a single cycle of the game
-//
-//Level::Level(char levelFile[])
-//{
-//	score = 0;
-//	loadLevel(levelFile);
-//}
+void drawNicholasMenu(int xres, int yres, Rect r)
+{
+    //Ship ship;
+    //ship.move();
+    //ship.collidesWith();
+    //ship.draw();
 
-	void drawNicholasMenu(int xres, int yres, Rect r)
-	{
-	    //Ship ship;
-	    //ship.move();
-	    //ship.collidesWith();
-	    //ship.draw();
+    //Ship ship;
+    //Enemy enemy(&ship);
+    //enemy.move();
+    //enemy.draw();
 
-	    //Ship ship;
-	    //Enemy enemy(&ship);
-	    //enemy.move();
-	    //enemy.draw();
+    //Ship ship;
+    //Chaser e1(&ship);
+    //e1.move();
+    //e1.draw();
 
-	    //Ship ship;
-	    //Chaser e1(&ship);
-	    //e1.move();
-	    //e1.draw();
+    //Ship ship;
+    //Circler e2(&ship);
+    //e2.move();
+    //e2.draw();
 
-	    //Ship ship;
-	    //Circler e2(&ship);
-	    //e2.move();
-	    //e2.draw();
+    //Asteroid ast;
+    //ast.move();
+    //ast.draw();
 
-	    //Asteroid ast;
-	    //ast.move();
-	    //ast.draw();
+    //Platform plat;
+    //plat.draw();
 
-	    //Platform plat;
-	    //plat.draw();
-
-	    //Level level1((char*)"This is a test");
-	    //level1.enemies[0] = e1;
-	    //level1.enemies[0].move();
-	    //level1.enemies[0].draw();
-
-	    glColor3f(1.0, 0.0, 0.0);
-	    int cx = xres/2;
-	    int cy = yres/2;
-	    glBegin(GL_QUADS);
-	    glVertex2f(cx-100, cy+100);
-	    glVertex2f(cx+100, cy+100);
-	    glVertex2f(cx+100, cy-100);
-	    glVertex2f(cx-100, cy-100);
-	    glEnd();
-	    glEnable(GL_TEXTURE_2D);
-	    r.bot = cy + 20;
-	    r.left = cx;
-	    r.center = 1;
-	    ggprint8b(&r, 16, 0x00ffff00, "This is Nick's menu");
-	    ggprint8b(&r, 16, 0x00ffff00, "Debugging!");
-	}
+    glColor3f(1.0, 0.0, 0.0);
+    int cx = xres/2;
+    int cy = yres/2;
+    glBegin(GL_QUADS);
+    glVertex2f(cx-100, cy+100);
+    glVertex2f(cx+100, cy+100);
+    glVertex2f(cx+100, cy-100);
+    glVertex2f(cx-100, cy-100);
+    glEnd();
+    glEnable(GL_TEXTURE_2D);
+    r.bot = cy + 20;
+    r.left = cx;
+    r.center = 1;
+    ggprint8b(&r, 16, 0x00ffff00, "This is Nick's menu");
+    ggprint8b(&r, 16, 0x00ffff00, "Debugging!");
+}
--- nicholasP.h
+++ nicholasP.h
@@ -16,9 +16,14 @@
 typedef float Vec[3];
 
 struct Shape {
-    float width, height;
+    // width + height = rectangle
+    // base + height = triangle
+    // radius = circle
+    float width;
+    float height;
+    float base;
     float radius;
-    Vec center;
+    Vec center; // Remove after integrate with rocketlander.cpp
 };
 
 class Object
@@ -73,8 +78,24 @@ class MovableObject : public Object
 
 // Rename to Ship when integrating into rocketlander.cpp
 class Ship2 : public MovableObject {
+//class Ship : public MovableObject {
 	private:
 		Shape collidables[5];
+		int fuel;
+
+		// Choose between the 3
+		bool haveBooster1;
+		bool enabledBooster1;
+		bool haveBooster2;
+		bool enabledBooster2;
+		bool haveBooster3;
+		bool enabledBooster3;
+		
+		bool haveLeftStrafe;
+		bool haveRightStrafe;
+		bool haveDoubleShot;
+		bool haveTripleShot;
+
 	public:
 		// Constructor
 		Ship2();
@@ -84,38 +105,52 @@ class Ship2 : public MovableObject {
 		void draw();
 		void drawRect();
 		void drawTri();
+
+		bool buyBooster1();
+		bool buyBooster2();
+		bool buyBooster3();
+
+		bool ownBooster1() {return haveBooster1;}
+		bool ownBooster2() {return haveBooster2;}
+		bool ownBooster3() {return haveBooster3;}
+
+		void enableBooster1();
+		void enableBooster2();
+		void enableBooster3();
 };
 
-//class Enemy : public MovableObject {
-//	protected:
-//		int health;
-//
-//	public:
-//		Enemy();
-//		void move();
-//		void draw();
-//};
-//
-//class Chaser : public Enemy {
-//	protected:
-//		Ship * ship;
-//
-//	public:
-//		Chaser(Ship*);
-//		void move();
-//		void draw();
-//};
-//
-//class Circler : public Enemy {
-//	protected:
-//		Ship * ship;
-//
-//	public:
-//		Circler(Ship*);
-//		void move();
-//		void draw();
-//};
-//
+class Enemy : public MovableObject {
+	protected:
+		int health;
+
+	public:
+		Enemy();
+		void move();
+		void draw();
+};
+
+class Chaser : public Enemy {
+	private:
+		Ship2 * ship;
+
+	public:
+		Chaser();
+		//Chaser(*Ship);
+		void move();
+		void draw();
+};
+
+class Circler : public Enemy {
+	private:
+		Ship2 * ship;
+
+	public:
+		Circler();
+		//Circler(*Ship);
+		void move();
+		void draw();
+};
+
 class Asteroid2 : public MovableObject {
 //class Asteroid : public MovableObject {
 	public:
@@ -125,36 +160,12 @@ class Asteroid2 : public MovableObject {
 
 class Platform : public Object
 {
+    private:
+	bool isGoal;
+	bool isRefuel;
     public:
+	Platform();
 	void draw();
-	//void draw() override;
 };
-
-//class Level {
-//	protected:
-//		float gravity;
-//		float time;
-//		int score;
-//		Ship ship;
-//		Enemy enemies[5];
-//		Asteroid asteroids[10];
-//		Platform platforms[100];
-//		int enemyCount;
-//		int asteroidCount;
-//
-//		void loadLevel(char[]);
-//		void moveObjects();
-//		void removeEnemy(int);
-//		void addEnemy(Enemy);
-//		void addAsteroid(Asteroid);
-//		void removeAsteroid(int);
-//		int calculateFinalScore();
-//		bool objectiveComplete();
-//
-//	public:
-//		void init_level();
-//		void runCycle();
-//		Level(char[]);
-//};
 
 void drawNicholasMenu(int xres, int yres, Rect r);
