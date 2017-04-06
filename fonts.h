184a06f50f212a114bb6f65a9d3737bd6a445da3
Abraham117 <Abraham117@users.noreply.github.com>
2017-04-06 11:06:02 -0700

    Update fonts.h

--- fonts.h
+++ fonts.h
@@ -13,6 +13,7 @@ typedef struct t_rect {
 } Rect;
 extern void initialize_fonts(void);
 extern void cleanup_fonts(void);
+extern void ggprint40(Rect *r, int advance, int cref, const char *fmt, ...);
 extern void ggprint16(Rect *r, int advance, int cref, const char *fmt, ...);
 extern void ggprint12(Rect *r, int advance, int cref, const char *fmt, ...);
 extern void ggprint13(Rect *r, int advance, int cref, const char *fmt, ...);
